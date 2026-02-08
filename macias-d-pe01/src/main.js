dbc33194d7fb283a53f3a951f5ada0c0a93c460e
Dania Macias <dlm4695@rit.edu>
2026-02-08 16:38:36 -0500

    converted all functions to arrow functions, imported randomElement to main.js

--- macias-d-pe01/src/main.js
+++ macias-d-pe01/src/main.js
@@ -1,29 +1,25 @@
-"use strict";
-		
+import { randomElement } from "./utils";
+
 const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
 
 const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
 
 const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
 
-init(); 
-
-function init() {
+const init = () => {
     generateTechno;
     document.querySelector("#btn-gen-1").addEventListener("click", () => generateTechno(1));
     document.querySelector("#btn-gen-5").addEventListener("click", () => generateTechno(5));
 }
 
-function randomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
-function generateTechno(num) {
+const generateTechno= (num) => {
     const outputDiv = document.querySelector("#output");
-
+    
     for (let i = 0; i < num; i++) {
         const newP = document.createElement('p');
         newP.textContent = `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
         outputDiv.appendChild(newP);
     }
 }	
+
+init(); 
