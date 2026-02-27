f9d3ce90c5d8d5ca788f24e573461d53b2e81f43
Dania Macias <dlm4695@rit.edu>
2026-02-26 23:52:48 -0500

    moved all js into external js file

---
+++ macias-d-screensaver-refactored/src/main.js
@@ -0,0 +1,196 @@
+let canvas;
+let ctx;
+
+let paused = false;
+let createRectangles = true;
+let createArcs = false;
+let createLines = false;
+
+let frameCount = 0;
+let drawDelay = 10;
+
+// Init function
+const init = () => {
+    canvas = document.querySelector("canvas");
+    ctx = canvas.getContext("2d");
+    
+    setupUI();
+    update();
+}
+
+// Set up UI buttons, checkboxes, and canvas click and clear
+const setupUI = () => {
+    document.querySelector("#btn-play").onclick = function() {
+        paused = false;
+    };
+
+    document.querySelector("#btn-pause").onclick = function() {
+        paused = true;
+    };
+
+    document.querySelector("canvas").onclick = canvasClicked;
+
+    document.querySelector("#cb-rectangles").onclick = function(e) {
+        createRectangles = e.target.checked;
+    }
+
+    document.querySelector("#cb-arcs").onclick = function(e) {
+        createArcs = e.target.checked;
+    }
+
+    document.querySelector("#cb-lines").onclick = function(e) {
+        createLines = e.target.checked;
+    }
+
+    document.querySelector("#btn-clear").onclick = function() {
+        ctx.clearRect(0, 0,canvas.width, canvas.height);
+    }
+}
+
+// Animation loop
+const update = () => {
+    requestAnimationFrame(update);
+    
+    if (paused) return;
+
+    frameCount++;
+
+    if (frameCount % drawDelay !== 0) return;
+
+    if (createRectangles) {
+        drawRandomRect(ctx); 
+    }
+
+    if (createArcs){
+        drawRandomArc();
+    }
+
+    if (createLines){
+        drawRandomLine();
+    }
+}
+
+// Helper functions
+const getRandomColor = () => {
+    const getByte = () => {
+        return 55 + Math.round(Math.random() * 200);
+}
+    return `rgba(${getByte()}, ${getByte()}, ${getByte()}, .5)`;
+}
+
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const canvasClicked = (e) => {
+    let rect = e.target.getBoundingClientRect();
+    let mouseX = e.clientX - rect.x;
+    let mouseY = e.clientY - rect.y;
+
+    for (let i = 0; i < 10; i++) {
+        let randX = mouseX + Math.random() * 50 - 10;
+        let randY = mouseY + Math.random() * 50 - 10;
+
+        let randRad = Math.random() * 10;
+
+        drawArc(ctx, randX, randY, randRad,"blue");
+    }
+}
+
+const drawRectangle = (ctx,x,y,width,height,fillStyle="black",lineWidth=0,strokeStyle="black") => {  
+    ctx.save();
+
+    ctx.fillStyle = fillStyle;
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.lineJoin = "round";
+
+    ctx.beginPath();
+    ctx.rect(x,y,width,height);
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.restore();
+}
+
+const drawArc = (ctx, x, y, radius, fillStyle="black", lineWidth=0, strokeStyle="black", startAngle=0,endAngle=Math.PI*2) => {
+    ctx.save();
+
+    ctx.fillStyle = fillStyle;
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.lineJoin = "round";
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.closePath();
+
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.restore();
+}
+
+const drawLine = (ctx, x1, x2, y1, y2, lineWidth=1, strokeStyle="black") => {
+    ctx.save();
+
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = lineWidth;
+    ctx.lineJoin = "round";
+
+    ctx.beginPath();
+    ctx.moveTo(x1, x2);
+    ctx.lineTo(y1, y2);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.restore();
+}
+
+const drawRandomRect = () => {
+    // Get random coordinates and width and height
+    let x = getRandomInt(0, canvas.width);
+    let y = getRandomInt(0, canvas.height);
+    let width = getRandomInt(20, 200);
+    let height = getRandomInt(20, 200);
+    let fillColor = getRandomColor();
+    let lineWidth = getRandomInt(1, 10);
+    let strokeStyle = getRandomColor();
+
+    // Draw rectangle
+    drawRectangle(ctx, x, y, width, height, fillColor, lineWidth, strokeStyle);
+}
+
+const drawRandomArc = () => {
+    // Get random values for coordinates and radius
+    let x = getRandomInt(0, canvas.width);
+    let y = getRandomInt(0, canvas.height);
+    let radius = getRandomInt(10, 100);
+
+    // Get random values for fill and stroke
+    let fillColor = getRandomColor();
+    let lineWidth = getRandomInt(1, 10);
+    let strokeStyle = getRandomColor();
+
+    // Draw arc
+    drawArc(ctx, x, y, radius, fillColor, lineWidth, strokeStyle);
+}
+
+const drawRandomLine = () => {
+    // Get random values for coordinates
+    let x1 = getRandomInt(0, canvas.width);
+    let y1 = getRandomInt(0, canvas.height);
+    let x2 = getRandomInt(0, canvas.width);
+    let y2 = getRandomInt(0, canvas.height);
+
+    // Get random values for stroke
+    let lineWidth = getRandomInt(1, 10);
+    let strokeStyle = getRandomColor();
+
+    // Draw line
+    drawLine(ctx, x1, y1, x2, y2, lineWidth, strokeStyle);
+}
+
+init();
