11fe0869a928315c000ba20e6b7dc2df7986358d
Dania Macias <dlm4695@rit.edu>
2026-03-11 19:14:44 -0400

    added animated bards and circles to the visualizer

--- web-audio-hw/src/main.js
+++ web-audio-hw/src/main.js
@@ -9,34 +9,25 @@
 
 import * as utils from './utils.js';
 import * as audio from './audio.js';
+import * as canvas from './canvas.js';
+
+const drawParams = {
+  showGradient  : true,
+  showBars      : true,
+  showCircles   : true,
+  showNoise     : true
+};
 
 const loop = () => {
 /* NOTE: This is temporary testing code that we will delete in Part II */
 	requestAnimationFrame(loop);
+  canvas.draw(drawParams);
 	// 1) create a byte array (values of 0-255) to hold the audio data
 	// normally, we do this once when the program starts up, NOT every frame
 	let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
 	
 	// 2) populate the array of audio data *by reference* (i.e. by its address)
 	audio.analyserNode.getByteFrequencyData(audioData);
-	
-	// 3) log out the array and the average loudness (amplitude) of all of the frequency bins
-		console.log(audioData);
-		
-		console.log("-----Audio Stats-----");
-		let totalLoudness =  audioData.reduce((total,num) => total + num);
-		let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
-		let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
-		let maxLoudness =  Math.max(...audioData); // ditto!
-		// Now look at loudness in a specific bin
-		// 22050 kHz divided by 128 bins = 172.23 kHz per bin
-		// the 12th element in array represents loudness at 2.067 kHz
-		let loudnessAt2K = audioData[11]; 
-		console.log(`averageLoudness = ${averageLoudness}`);
-		console.log(`minLoudness = ${minLoudness}`);
-		console.log(`maxLoudness = ${maxLoudness}`);
-		console.log(`loudnessAt2K = ${loudnessAt2K}`);
-		console.log("---------------------");
 }
 
 // 1 - here we are faking an enumeration
@@ -51,7 +42,8 @@ const init = () =>{
 	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
 	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
 	setupUI(canvasElement);
-    loop();
+  canvas.setupCanvas(canvasElement,audio.analyserNode);
+  loop();
 }
 const setupUI = (canvasElement) => {
   // A - hookup fullscreen button
