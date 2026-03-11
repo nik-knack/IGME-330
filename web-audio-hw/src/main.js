916b938f8dac6d37466ecba66a0419768a9fc087
Dania Macias <dlm4695@rit.edu>
2026-03-11 16:28:55 -0400

    added loop code

--- web-audio-hw/src/main.js
+++ web-audio-hw/src/main.js
@@ -10,6 +10,35 @@
 import * as utils from './utils.js';
 import * as audio from './audio.js';
 
+const loop = () => {
+/* NOTE: This is temporary testing code that we will delete in Part II */
+	requestAnimationFrame(loop);
+	// 1) create a byte array (values of 0-255) to hold the audio data
+	// normally, we do this once when the program starts up, NOT every frame
+	let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
+	
+	// 2) populate the array of audio data *by reference* (i.e. by its address)
+	audio.analyserNode.getByteFrequencyData(audioData);
+	
+	// 3) log out the array and the average loudness (amplitude) of all of the frequency bins
+		console.log(audioData);
+		
+		console.log("-----Audio Stats-----");
+		let totalLoudness =  audioData.reduce((total,num) => total + num);
+		let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
+		let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
+		let maxLoudness =  Math.max(...audioData); // ditto!
+		// Now look at loudness in a specific bin
+		// 22050 kHz divided by 128 bins = 172.23 kHz per bin
+		// the 12th element in array represents loudness at 2.067 kHz
+		let loudnessAt2K = audioData[11]; 
+		console.log(`averageLoudness = ${averageLoudness}`);
+		console.log(`minLoudness = ${minLoudness}`);
+		console.log(`maxLoudness = ${maxLoudness}`);
+		console.log(`loudnessAt2K = ${loudnessAt2K}`);
+		console.log("---------------------");
+}
+
 // 1 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
 	sound1  :  "media/New Adventure Theme.mp3"
@@ -22,6 +51,7 @@ const init = () =>{
 	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
 	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
 	setupUI(canvasElement);
+    loop();
 }
 const setupUI = (canvasElement) => {
   // A - hookup fullscreen button
