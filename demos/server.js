ff80b622841a5e5547d8b4ef523fff466227e589
Dania Macias <dlm4695@rit.edu>
2026-03-20 10:50:58 -0400

    commit changes

--- demos/server.js
+++ demos/server.js
@@ -6,10 +6,12 @@ const server = createServer((req, res) => {
     const ip = req.socket.remoteAddress;
     console.log(`${req.method} ${req.url} - from ${ip}`);
 
+    ips.add(ip);
+
     res.writeHead(200, {
         "Content-Type": "text/plain"
     });
-    res.end(`You hit: ${req.url}\n`);
+    res.end([...ips].join("\n"));
 });
 
 server.listen(PORT, "0.0.0.0", () => {
