45676ad48addc02edeaab57cc171e112174552ed
Dania Macias <dlm4695@rit.edu>
2026-03-30 10:51:07 -0400

    fnished 3/30 class demo

--- demos/mar_25/src/main.ts
+++ demos/mar_25/src/main.ts
@@ -1,60 +1,27 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+// const add = (a: string, b: string): string => {
+//   return (a + b);  // unknown: could be any and all values
+// }
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+// const message: string = add('1', '2');  // string not a number
 
-<div class="ticks"></div>
+// console.log(message);
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src=${viteLogo} alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+// npm install to reintsall node_modules
+// npm run dev to run vite project on browser
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+type GameEvents = 'key_press' | 'mouse_press' | 'point_gained' | 'bullet_collision';  // doesn't compile 
+// // | type union operator
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let evt: GameEvents = 'key_press';
+
+type KeyPressEvent = {
+  type: 'key_press',
+  key: string,
+  times: number
+}
+
+const keypressEvent: KeyPressEvent = {
+  type: 'key_press',
+  times: 34,
+  key: 'A'
+}
